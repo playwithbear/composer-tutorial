3c2d886232bbe4e9a001915ece37c991d8ef2858
playwithbear <goldspinky@hotmail.com>
2019-09-11 15:12:01 +0100

    added ini for security

--- send.php
+++ send.php
@@ -5,6 +5,10 @@ require_once 'vendor/autoload.php';
 use League\Csv\Reader;
 use GuzzleHttp\Client;
 
+$ini = parse_ini_file("server.ini",true);
+
+$ini = $ini['server-details']['url'];
+
 //load the CSV document from a file path
 $csv = Reader::createFromPath('MOCK_DATA.csv', 'r');
 $csv->setHeaderOffset(0);
@@ -16,7 +20,7 @@ $records = $csv->getRecords(); //returns all the CSV records as an Iterator obje
 
 $client = new Client([
     // Base URI is used with relative requests
-    'base_uri' => 'http://requestbin.net/r/1gl24891',
+    'base_uri' => $ini,
     // You can set any number of default request options.
     'timeout'  => 2.0,
 ]);
